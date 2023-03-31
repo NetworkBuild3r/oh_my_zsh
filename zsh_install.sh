31060c2c82dd6f3b27f88b41c06c1ed65d0aeb16
Brian Nelson <103077009+NetworkBuild3r@users.noreply.github.com>
2023-03-31 11:09:14 -0500

    Update zsh_install.sh

--- zsh_install.sh
+++ zsh_install.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 
 # Install Git and Zsh
-sudo apt-get update && sudo apt-get install -y git zsh
+echo "Installing Git and Zsh..."
+sudo apt-get update || { echo "Failed to update packages. Please check your network connection and try again." >&2; exit 1; }
+sudo apt-get install -y git zsh || { echo "Failed to install Git and Zsh. Please check your network connection and try again." >&2; exit 1; }
 
 # Clone Prezto if ~/.zprezto doesn't exist
 if [ ! -d ~/.zprezto ]; then
-    git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+    echo "Cloning Prezto..."
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto || { echo "Failed to clone Prezto. Please check your network connection and try again." >&2; exit 1; }
 fi
 
-# Backup and link zsh config files
-link_file() {
-    file=$1
-    if [ -e ~/$file ]; then
-        if [ ! -L ~/$file ]; then
-            mv ~/$file ~/$file.backup
-        else
-            unlink ~/$file
-        fi
-    fi
-    ln -s ~/.zprezto/runcoms/$file ~/$file
-}
+# Backup zsh config if it exists
+if [ -f ~/.zshrc ]; then
+    echo "Backing up existing ~/.zshrc file..."
+    mv ~/.zshrc ~/.zshrc.backup || { echo "Failed to backup ~/.zshrc file. Please check your file permissions and try again." >&2; exit 1; }
+fi
 
-# Link zsh config files
-link_file zlogin
-link_file zlogout
-link_file zpreztorc
-link_file zprofile
-link_file zshenv
-link_file zshrc
+# Create links to zsh config files
+echo "Creating links to zsh config files..."
+ln -s ~/.zprezto/runcoms/zlogin ~/.zlogin || { echo "Failed to create link for ~/.zlogin file. Please check your file permissions and try again." >&2; exit 1; }
+ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout || { echo "Failed to create link for ~/.zlogout file. Please check your file permissions and try again." >&2; exit 1; }
+ln -s ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc || { echo "Failed to create link for ~/.zpreztorc file. Please check your file permissions and try again." >&2; exit 1; }
+ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile || { echo "Failed to create link for ~/.zprofile file. Please check your file permissions and try again." >&2; exit 1; }
+ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv || { echo "Failed to create link for ~/.zshenv file. Please check your file permissions and try again." >&2; exit 1; }
+ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc || { echo "Failed to create link for ~/.zshrc file. Please check your file permissions and try again." >&2; exit 1; }
 
-# Update zpreztorc with custom configuration
-curl -o ~/.zprezto/runcoms/zpreztorc https://raw.githubusercontent.com/Dan70402/zprezto/master/.zpreztorc
+# Source the updated zsh configuration
+echo "Sourcing the updated zsh configuration..."
+source ~/.zshrc || { echo "Failed to source the updated zsh configuration. Please check your file permissions and try again." >&2; exit 1; }
 
-# Update source line in ~/.zshrc to source the new zpreztorc file
-sed -i 's/source $ZPREZTODIR\/runcoms\/zpreztorc/source ~\/.zprezto\/runcoms\/zpreztorc/g' ~/.zshrc
-
-# Source the updated .zshrc file
-source ~/.zshrc
+echo "oh-my-zsh with Prezto theme and plugins has been installed successfully!"
+echo "You may need to restart your terminal or run 'source ~/.zshrc' to see the changes."
